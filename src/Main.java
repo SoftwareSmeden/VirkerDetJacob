cfcc299def4ab9f2dc65066afa98aa779922dea1
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2022-02-21 11:24:53 +0100

    Test branch med ny linje

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Det virker sgu, Torben!");
 
         System.out.println("Hej Susanne. Virker det mon med en branch?");
+        System.out.println("Ny linje... :)");
     }
 }
